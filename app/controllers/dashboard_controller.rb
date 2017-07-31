02032a449fea28875ffcbd34c2798fca35d7ed75
CoffeeAndCream <seanjhulse@gmail.com>
2017-07-31 15:57:14 -0500

    Work and stuff

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,7 @@
 class DashboardController < ApplicationController
- include Secured
+  before_action :authenticate_user!
 
   def show
+    @user = current_user
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  private
+  def user_signed_in?
+    session[:userinfo].present?
+  end
+  # Set the @current_user or redirect to public page
+  def authenticate_user!
+    # Redirect to page that has the login here
+    if user_signed_in?
+      @current_user = session[:userinfo]
+    else
+      redirect_to login_path
+    end
+  end
+  def current_user
+    @current_user
+  end
+  def login_path
+    root_path
+  end
 end
